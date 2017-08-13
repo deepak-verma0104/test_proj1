8a7fcf5958255d07b9f70c9e63dfe3795133e1ee
deepak-verma0104 <vermadkv@yahoo.com>
2017-08-13 04:15:51 -0700

    added 2nd function in my_branch1

--- src/cubeprog.py
+++ src/cubeprog.py
@@ -5,4 +5,12 @@ def my_cube(x):
 
     return x*x*x
 
+def my_cube2(x):
+    """ This program finds the cube of 6 
+	This is another line added in the comments
+    """
+
+    return x*x*x
+
 print(my_cube(10))
+print(my_cube(6))
